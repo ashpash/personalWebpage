a3ae9903002d067a762f0640b1fb1801510cefe6
Ashley Bernhardt <ashleybernhardt@hotmail.com>
2018-06-04 17:13:43 -0800

    hex

---
+++ ashBern.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>AshleyBernhardt</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/main.css" />
+  </head>
+
+  <body>
+    <div id="main">
+      <header>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+      </header>
+    </div>
+
+
+    <nav class="navbar navbar-inverse navbar-static-top">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">Ashley Bernhardt</a>
+		</div>
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="About.php">About</a></li>
+			  <li><a href="Resume.php">Resume</a></li>
+        <li><a href="Projects.php">Projects</a></li>
+				<li><a href="Contact.php">Contact</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+    <div class="container" id="hex">
+      <div class="heart">
+        <ul class="list-unstyled"><li><h4>Ashley Bernhardt</h4></li>
+          <li><h4>1329 Ellis Street, Kelowna B.C., V1Y 9X5</h4></li>
+        </ul>
+        <p class="hamburger">
+
+        I am a responsible, personable and self-motivated individual. My previous work experience involves self-directed tasks, problem solving and providing excellent customer service. I enjoy contributing to both a team environment, and I also possess the work ethic to excel independently. I thrive in many different work situations. I am looking for an opportunity where I can provide friendly and attentive services for customers.
+
+        Employment:
+
+        Lifeguarding – H2O Recreation and Aquatics center, May 2009 – Feb 2011
+        Provided lifeguarding services, first aid, pool maintenance, and guest assistance.
+
+        Swim Instructor- YMCA, September 2009 – Feb 2011
+        Created detailed lesson plans, instructed children ages 4 to 12, and worked with parents.
+        La Bussola- March 2011 - Present
+        Bussing, hostessing, serving, and dish washing
+        Education:
+        University of British Columbia Okanagan - Business Management Program
+        2010 – Present (to graduate in 2014)
+        Okanagan Mission Secondary- Kelowna, B.C.
+        Graduated in 2010 with a 3.5 GPA
+
+        Skills:
+
+        Adaptable, with the ability to learn new skills quickly
+        Organized and efficient
+        Enjoys working with others
+        Personable and considerate of customer’s needs
+        Computer skills, including Microsoft word, Excel, PowerPoint and Outlook
+
+         References:
+        Gladly provided upon request.
+
+      </p>
+    </div>
+  </div>
+
+    <footer id="bottom">
+    </footer>
+
+  </body>
+
+</html>
